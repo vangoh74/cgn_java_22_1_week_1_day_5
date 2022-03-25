df4a03a10051d320ab0c7470d3eaabbfa4c460e4
Vandicleia Goncalves <vangoh05@protonmail.com>
2022-03-25 12:35:33 +0100

    First part of the project

--- src/main/java/PasswordValidation.java
+++ src/main/java/PasswordValidation.java
@@ -1,2 +1,49 @@
+import java.util.Scanner;
+
 public class PasswordValidation {
+    public static void main(String[] args) {
+        String pw = getPassword();
+        if (isPasswordValid(pw)) {
+            System.out.println("Dein Passwort ist gültig!");
+        }
+        System.out.println("Passwort ist falsch!");
+    }
+
+    /* Conditions:
+        #1 Min Length: 8
+        #2 Contains
+    * */
+    public static boolean isPasswordValid(String password) {
+        int minLength = 8;
+        for (char c : password.toCharArray()) {
+
+            if (isDigit(c) && checkLength(password)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean checkLength(String str) {
+        int minLength = 8;
+        if (str.length() >=  minLength) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isDigit(char ch) {
+        if (Character.isDigit(ch)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static String getPassword() {
+        System.out.println("Bitte, geben Sie Ihr Password ein: ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        return input;
+    }
 }
+
---
+++ src/test/java/PasswordValidationTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordValidationTest {
+
+    @Test
+    void isPasswordValid_WhenLengthAndisDigitThan_shouldReturnTrue() {
+        assertTrue(PasswordValidation.isPasswordValid("asfik9set"));
+    }
+
+    @Test
+    void isPasswordValid_WhenLengthAndisDigitThan_shouldReturnFalse() {
+        assertFalse(PasswordValidation.isPasswordValid("asfikset"));
+    }
+
+    @Test
+    void checkLength_WhenMinLengthEightThan_shouldReturnTrue() {
+        assertTrue(PasswordValidation.isPasswordValid("asfik9set"));
+    }
+
+    @Test
+    void checkLength_WhenLengthEightThan_shouldReturnFalse() {
+        assertFalse(PasswordValidation.isPasswordValid("asfik9"));
+    }
+
+    @Test
+    void isDigit_WhenContainsMinOneDigitThan_shouldReturnTrue() {
+        assertTrue(PasswordValidation.isPasswordValid("asfik9set"));
+    }
+
+    @Test
+    void isDigit_WhenNoDigitThan_shouldReturnFalse() {
+        assertFalse(PasswordValidation.isPasswordValid("asfiksfaset"));
+    }
+}
